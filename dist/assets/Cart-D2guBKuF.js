1ca0e5eb565e57996b41c1d2af34c3cf3de1faed
Kls.py <131408936+Kaua-Klassmann@users.noreply.github.com>
2024-03-10 13:55:38 -0300

    Delete dist directory

--- dist/assets/Cart-D2guBKuF.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as r}from"./index-BM0wrY53.js";const t={};function n(o,a){return r(),c("div",null,"Cart")}const _=e(t,[["render",n]]);export{_ as default};
--- dist/assets/Favorites-Bd4Ay7H9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-BM0wrY53.js";const r={};function t(n,s){return o(),c("div",null,"Favorites")}const _=e(r,[["render",t]]);export{_ as default};
